408211355c4939f08f99adc9d717e86382783a2a
Ghosty <104216517+lewis6777@users.noreply.github.com>
2026-04-27 21:34:44 +0100

    Add files via upload

---
+++ AddTroopText.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace TopTroops.Patches
+{
+    [HarmonyPatch(typeof(GorillaNetworking.GorillaComputer), "TroopScreen")]
+    class AddTroopText
+    {
+        static void Postfix(GorillaNetworking.GorillaComputer __instance)
+        {
+            var field = AccessTools.Field(__instance.GetType(), "screenText");
+            var screenText = field.GetValue(__instance);
+
+            var appendMethod = screenText.GetType().GetMethod("Append", new[] { typeof(string) });
+
+            appendMethod.Invoke(screenText, new object[] { $"\nTOP TROOPS: {string.Join(" ", Plugin.topTroops)}" });
+        }
+    }
+}
---
+++ GetTopTroops.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+
+namespace TopTroops.Patches
+{
+    [HarmonyPatch(typeof(GorillaNetworking.GorillaComputer))]
+    [HarmonyPatch("OnReturnCurrentVersion")]
+    class GetTopTroops
+    {
+        static void Postfix(GorillaNetworking.GorillaComputer __instance)
+        {
+            var field = AccessTools.Field(typeof(GorillaNetworking.GorillaComputer), "topTroops");
+            var value = field.GetValue(__instance) as List<string>;
+            if (value == null) return;
+            Plugin.topTroops = new List<string>(value);
+        }
+    }
+}
