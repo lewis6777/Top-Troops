4288c6aec3b601f037bc454a4f2595d54ba8cebe
Ghosty <104216517+lewis6777@users.noreply.github.com>
2026-04-27 21:33:37 +0100

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
+using UnityEngine;
+using GorillaNetworking;
+
+namespace TopTroops
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static List<string> topTroops = new List<string>();
+
+        void OnEnable()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+
+        void OnDisable()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace TopTroops
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "com.ghosty.gorillatag.toptroops";
+        public const string Name = "TopTroops";
+        public const string Version = "1.0.0";
+    }
+}
